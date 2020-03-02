cd5f201d177d57c9d1af6c208fc5f544516e16ef
jockf <jockfindlay@gmail.com>
2020-03-02 15:15:17 +0000

    Fav-icons

---
+++ assets/img/CreateAssetsFromSource.bat
@@ -0,0 +1,12 @@
+convert Icon32.png -filter point -resize 16x16 dwerg-saga-16x16.png
+convert Icon32.png -filter point -resize 32x32 dwerg-saga-32x32.png
+convert Icon32.png -filter point -resize 96x96 dwerg-saga-96x96.png
+
+convert Icon32.png -filter point -resize 120x120 dwerg-saga-120x120.png
+convert Icon32.png -filter point -resize 152x152 dwerg-saga-152x152.png
+convert Icon32.png -filter point -resize 167x167 dwerg-saga-167x167.png
+convert Icon32.png -filter point -resize 180x180 dwerg-saga-180x180.png
+
+convert Icon32.png -filter point -resize 70x70 dwerg-saga-70x70.png
+convert Icon32.png -filter point -resize 150x150 dwerg-saga-150x150.png
+convert Icon32.png -filter point -resize 310x310 dwerg-saga-310x310.png
