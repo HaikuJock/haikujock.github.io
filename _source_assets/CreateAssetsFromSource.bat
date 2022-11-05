506cb2a8f2e9b92d15edf9a1c06ff159a7da8467
jockf <jockfindlay@gmail.com>
2022-11-05 14:26:54 +0000

    New logo

---
+++ _source_assets/CreateAssetsFromSource.bat
@@ -0,0 +1,14 @@
+convert Logo_Symbol.png -resize 16x16 ../assets/img/dwerg-saga-16x16.png
+convert Logo_Symbol.png -resize 32x32 ../assets/img/dwerg-saga-32x32.png
+convert Logo_Symbol.png -resize 96x96 ../assets/img/dwerg-saga-96x96.png
+convert Logo_Symbol.png -resize 120x120 ../assets/img/dwerg-saga-120x120.png
+convert Logo_Symbol.png -resize 152x152 ../assets/img/dwerg-saga-152x152.png
+convert Logo_Symbol.png -resize 167x167 ../assets/img/dwerg-saga-167x167.png
+convert Logo_Symbol.png -resize 180x180 ../assets/img/dwerg-saga-180x180.png
+convert Logo_Symbol.png -resize 70x70 ../assets/img/dwerg-saga-70x70.png
+convert Logo_Symbol.png -resize 150x150 ../assets/img/dwerg-saga-150x150.png
+convert Logo_Symbol.png -resize 310x310 ../assets/img/dwerg-saga-310x310.png
+
+convert Logo_Horizontal.png -resize 310x150 ../assets/img/dwerg-saga-wide-310x150.png
+
+convert Logo_Vertical.png -resize 512x476 ../assets/img/DwergSagaLogo.png
